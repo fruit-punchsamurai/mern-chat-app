39b786d66cf76dfd9e4acd5bdebae0879c257147
Nimesh Gopal Pradhan <nimeshgpradhan@gmail.com>
2025-05-01 00:10:07 +0545

    Connect node with mongoDb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,29 @@
-const express = require("express")
-const {chats} = require("./data/data")
+const express = require("express");
+const { chats } = require("./data/data");
 const cors = require("cors");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+connectDB();
 
-const app = express()
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/chats', (req,res) => {
-  res.json(chats)
-})
+app.get("/api/chats", (req, res) => {
+  res.json(chats);
+});
 
-app.get('/api/chats/:id', (req,res) => {
-  const chat = chats.find(c=>c._id === req.params.id)
-  res.send(chat)
-})
+app.get("/api/chats/:id", (req, res) => {
+  const chat = chats.find((c) => c._id === req.params.id);
+  res.send(chat);
+});
 
-app.get('/',(req,res) => {
-  res.send("API is running successfully")
-})
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started at Port: ${PORT}`))
+app.listen(PORT, console.log(`Server started at Port: ${PORT}`));
